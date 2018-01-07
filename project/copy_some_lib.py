2b98042fbff4a67f205d82f7ae9e6c1a9a1d70b0
sawyer <1103031078@qq.com>
2018-01-07 23:52:19 +0800

    fix something again

--- project/copy_some_lib.py
+++ project/copy_some_lib.py
@@ -1,5 +1,6 @@
 #备份数据库
 import pymongo
+import json
 
 client = pymongo.MongoClient()
 new_ganjiLib = client['new_ganjiLib']
@@ -7,9 +8,17 @@ links_lib = new_ganjiLib['links_lib']
 real_links = new_ganjiLib['real_links']
 detail = new_ganjiLib['detail']
 copy_version = new_ganjiLib['copy_version']
+good_details = new_ganjiLib['good_details']
 
-for i in detail.find():
-    copy_version.insert_one(i['massages'])
-
-for i in copy_version.find():
-    print(i)
+# for i in detail.find():
+#     copy_version.insert_one(i['massages'])
+#
+# for i in copy_version.find():
+#     print(i)
+#
+with open('/Users\sawyer\Desktop/sample.json', 'r', encoding='utf-8') as file:
+    all = file.readlines()
+    json = json.load(all)
+    for i in json:
+        print(i['cates'])
+    file.close()
